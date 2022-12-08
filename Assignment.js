85bc16394d0ca73f6d5396a08c3156bbdc2419b6
Deepak Nath <nathd3191@gmail.com>
2022-12-08 16:22:15 +0530

    Read

---
+++ Assignment.js
@@ -0,0 +1,31 @@
+
+var n = 6;
+var i;
+var flag = true;
+for (i = 2; i < n; i++) 
+        flag = false;
+        break;
+    }
+}
+if (flag) {
+    console.log("It Is Prime Number");
+} else {
+    console.log("It is not Prime Number");
+}
+
+
+// 2. Paliondrome String
+let str = "MADAM";
+
+let len = str.length;
+for (let i = 0; i ; i++) {
+    if == str[len - 1 - i]) {
+        flag = false;
+    }
+
+}
+if (flag) {
+    console.log("Palindrome");
+} else {
+    console.log("Not Palindrome");
+}
